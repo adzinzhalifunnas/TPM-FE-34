3ef1939130dd4a44b09b21b6b10a64addc0879cc
Dzin <adzinzhalifunnas@gmail.com>
2022-12-20 15:29:20 +0700

    Add Pertemuan 2 and Pertemuan 3, Update Docs/* and README

---
+++ Pertemuan 3/assets/js/script.js
@@ -0,0 +1,110 @@
+// var, let, const
+// function a(){
+//     if(true) {
+//         let x = 10
+//     }
+//     console.log(x)
+// }
+// a()
+// let x = 10
+// x = 20
+// console.log(x)
+
+// const y = 25;
+// console.log(y)
+
+// String, number, boolean,
+// var s = "Hello World!";
+// var s2 = 'Hello World! ' + s + '\n';
+// var s3 = `Hello World! ${s2}\n`
+
+// console.log(s,s2,s3)
+
+// const i = 5;
+// const f = 5.9;
+
+// const numberString = i + f + s
+// const numberString2 = s + i + f
+// console.log(numberString)
+// console.log(numberString2)
+
+// let isTrue = true
+// isTrue = !isTrue
+// console.log(isTrue)
+
+// + * / - Arithmethic
+// == >= <= === !=
+// let sum = 5 + 10;
+// sum += 10
+
+// const a = "2";
+// const b = 2;
+// const c = 3;
+// const d = 10;
+
+// If Else
+// if(a === b) console.log("A Equals B")
+// else console.log("A is not equal B")
+
+// if(c >= d) console.log("C greater than D")
+// else if(c <= d) console.log("C less than D")
+
+// if(a == b){
+//     console.log("a equals b")
+// }else{
+//     console.log("false")
+// }
+
+// Switch Case
+// const switchVar = 1;
+// switch(switchVar){
+//     case 1:
+//         console.log("Hello!");
+//         break;
+//     case 2:
+//         console.log("Hello World!");
+//         break;
+//     default:
+//         console.log("Value not found!");
+//         break;
+// }
+
+// Ternary
+// console.log((a == b) ? "a equals b" : "a not equals b")
+
+// Looping
+// for(let i = 1; i <= 100; i++){
+//     if(i % 2 != 0){
+//         console.log(i)
+//     }
+// }
+
+// let i = 1;
+// while(i <= 100){
+//     if(i % 2 != 0){
+//         console.log(i)
+//     }
+//     i++
+// }
+
+// let i = 1
+// do{
+//     if(i % 2 != 0){
+//         console.log(i)
+//     }
+//     i++
+// }while(i <= 100)
+
+// console.log(sumTwoNumber(10,22))
+// console.log(multiplyTwoNumber(2,5)) Reference Error
+
+// function sumTwoNumber(x, y){
+//     return x + y
+// }
+
+// const multiplyTwoNumber = function(x, y){
+//     return x * y
+// }
+
+// console.log(sumTwoNumber(3,8))
+// console.log(multiplyTwoNumber(2,5))
--- README.md
+++ README.md
@@ -23,12 +23,31 @@ git clone https://github.com/adzinzhalifunnas/TPM-FE-34.git
 
 ### Daftar Pertemuan
 - [Pertemuan 1](#pertemuan-1)
+- [Pertemuan 2](#pertemuan-2)
+- [Pertemuan 3](#pertemuan-3)
 
 ### Pertemuan 1
 - [ ] Topic: TPM FE
 - [ ] Time: 15 December 2022, 21.20 WIB - 23.00 WIB
+- [ ] Preview Link: [https://tpm.adzinz.com/p1/index.html](https://tpm.adzinz.com/p1/index.html)
 - [ ] Preview Screenshot
 ![Pertemuan 1](Docs/Pertemuan-1.png)
+
+### Pertemuan 2
+- [ ] Topic: TPM FE Pertemuan 2
+- [ ] Time: 17 December 2022, 21.20 WIB - 23.00 WIB
+- [ ] Preview Link: [https://tpm.adzinz.com/p2/index.html](https://tpm.adzinz.com/p2/index.html)
+- [ ] Preview Screenshot
+![Pertemuan 2](Docs/Pertemuan-2.png)
+
+### Pertemuan 3
+- [ ] Topic: TPM FE Pertemuan 3
+- [ ] Time: 19 December 2022, 21.20 WIB - 23.00 WIB
+- [ ] Preview Link: [https://tpm.adzinz.com/p3/index.html](https://tpm.adzinz.com/p3/index.html)
+- [ ] Preview Screenshot
+![Pertemuan 3a](Docs/Pertemuan-3a.png)
+![Pertemuan 3b](Docs/Pertemuan-3b.png)
+![Pertemuan 3c](Docs/Pertemuan-3c.png)
 
 ### Connect with Me
 - [ ] [Personal Website](https://adzinz.com/)
